6d48bad279f2588e6ea76bad381077a230aaddd7
Daniela Ortega1 <danielasuarezb.03@gmail.com>
2024-09-27 10:22:57 -0500

    finlly commit

--- telepi_herlper.py
+++ telepi_herlper.py
@@ -1,12 +1,18 @@
+
 import mysql.connector
 import os
 import streamlit as st
 
+
 def insert_merged_data_in_bulk(df, table_name='ClientesPQRS'):
+
+
     connection = None
     cursor = None
 
+
     try:
+
         connection = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
@@ -14,9 +20,13 @@ def insert_merged_data_in_bulk(df, table_name='ClientesPQRS'):
             database=os.getenv("DB_NAME")
         )
 
+
         if connection.is_connected():
+
             st.success("Connected to the database successfully.")
+
             cursor = connection.cursor()
+
 
             insert_query = f"""
             INSERT INTO {table_name} (IdCliente, NombreCompleto, Sexo, Edad, Ciudad, Idpqrs, Tipo, FechaCaso, Asunto, Estado, FechaCierre, Urgencia)
@@ -25,14 +35,19 @@ def insert_merged_data_in_bulk(df, table_name='ClientesPQRS'):
 
             merged_data = df[['IdCliente', 'NombreCompleto', 'Sexo', 'Edad', 'Ciudad', 'Idpqrs', 'Tipo', 'FechaCaso', 'Asunto', 'Estado', 'FechaCierre', 'Urgencia']].to_records(index=False).tolist()
 
+
             cursor.executemany(insert_query, merged_data)
+
+
             connection.commit()
+
 
             st.success(f"{cursor.rowcount} rows inserted successfully.")
 
     except mysql.connector.Error as e:
         st.error(f"Error: {e}")
         if connection:
+
             connection.rollback()
 
     finally:
@@ -40,3 +55,4 @@ def insert_merged_data_in_bulk(df, table_name='ClientesPQRS'):
             cursor.close()
         if connection and connection.is_connected():
             connection.close()
+
